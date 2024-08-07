c6699ab8632a908b81a0398b3ec40410070aff03
meanturtle <33424081+meanturtle@users.noreply.github.com>
2024-08-07 16:36:24 +0100

    jsontrick

--- Receive-Alert/run.ps1
+++ Receive-Alert/run.ps1
@@ -5,9 +5,6 @@ param($Request, $TriggerMetadata)
 
 Write-Host "Processing Webhook for Alert $($Request.Body.alertUID)"
 
-Write-Host "Does this work $($Request.Body.alertUID)"
-
-
 $HaloClientID = $env:HaloClientID
 $HaloClientSecret = $env:HaloClientSecret
 $HaloURL = $env:HaloURL
